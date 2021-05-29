aeb911eeec7e91544d0f358a94afd64fa8c591e7
Parthkomalwad <57681207+Parthkomalwad@users.noreply.github.com>
2021-05-29 17:54:57 +0530

    Upload History

---
+++ Assets/Sxriptes/EncodingAndDecoding.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Text;
+using UnityEngine.UI;
+using TMPro;
+using System.Globalization;
+
+public class EncodingAndDecoding : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI txt;
+    public string inputString;
+    [SerializeField] TextMeshProUGUI outputtxt;
+    
+   
+
+    public void Update() 
+    {
+       
+    }
+
+    public void Start()
+    {
+        
+    }
+
+    public void encoding()
+    {
+        inputString = txt.text.ToString();
+        List<char> charList = new List<char>();
+        string encodingstring = "";
+        charList.AddRange(inputString.ToCharArray());
+        foreach (char character in charList)
+        {
+            int i = (int)character + 5;
+            encodingstring = encodingstring + ((char)i);
+
+        }
+        outputtxt.text = encodingstring;
+        
+
+    }
+    public void Decoding()
+    {
+        inputString = txt.text.ToString();
+        List<char> charList = new List<char>();
+        string encodingstring = "";
+        charList.AddRange(inputString.ToCharArray());
+        foreach (char character in charList)
+        {
+            int i = (int)character - 5;
+            encodingstring = encodingstring + ((char)i);
+
+        }
+        outputtxt.text = encodingstring;
+       
+    }
+    public void keyboradOpening()
+    {
+        TouchScreenKeyboard keyborad;
+        keyborad = TouchScreenKeyboard.Open("", TouchScreenKeyboardType.Default);
+    }
+    public void clipBoardCopy()
+    {
+
+        inputString = outputtxt.text;
+        txt.text = inputString;
+    }
+
+
+}
+
---
+++ Assets/Sxriptes/Loadsceen.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Loadsceen : MonoBehaviour
+{
+    public void LoadNextScene() {
+        SceneManager.LoadScene(1);
+
+    }
+    public void LoadFirstScene()
+    {
+        SceneManager.LoadScene(0);
+
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
